6d47cf5b3340ad2914a93f53f0ec85cfee361e41
taku matsunaga <skyline199306@yahoo.co.jp>
2020-11-09 23:41:33 +0900

    commit all

---
+++ yabou/js/title.js
@@ -0,0 +1,30 @@
+'use strict';
+// import {fadeIn, fadeOut} from './fade.js';
+
+let timeCount = 0;
+
+let addBlur = function(){
+  document.getElementById('blurPoint').classList.add('blur');
+}
+
+setTimeout(addBlur, 5000);
+
+let addImg = function(){
+  document.getElementById('matsunagaImg').classList.add('matsunaga');
+  document.getElementById('matsunagaImg').insertAdjacentHTML('afterbegin', "<img src='/yabou/img/matsunaga.jpg' alt=''>");
+}
+
+setTimeout(addImg, 7000);
+
+let addTitle = function(){
+  document.getElementById('matsunagaTitle').classList.add('titleMatsunaga');
+  document.getElementById('matsunagaTitle').insertAdjacentHTML('afterbegin', "<div class='titleYabou'>松永の野望</div><div class='titleBtn'><a href='/yabou/index.html'>Play</a></div>");
+}
+
+setTimeout(addTitle, 7000)
+
+// $(function(){
+//   setTimeout(function(){
+//   $('.backSection').add(document.getElementById('blurPoint')).addClass('blur');
+//   },3000)
+// });
